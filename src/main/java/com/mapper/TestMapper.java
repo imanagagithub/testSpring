8df53750cf37341dcec82520c87494ab8e7327e2
imanagagithub <imanagagithub@gmail.com>
2017-10-14 18:40:33 +0900

    XMLファイル参照設定完了

--- src/main/java/com/mapper/TestMapper.java
+++ src/main/java/com/mapper/TestMapper.java
@@ -2,12 +2,9 @@ package com.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Select;
-
 import com.domain.TestBean;
 
 public interface TestMapper {
 
-	@Select("SELECT * FROM TEST")
 	List<TestBean> getTestBean();
 }
