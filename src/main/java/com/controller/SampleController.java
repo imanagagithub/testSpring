cf4ccefb207eee143fffe0a0b4240a8da4a03114
imanagagithub <imanagagithub@gmail.com>
2017-10-14 17:42:02 +0900

    MySQL接続設定完了

--- src/main/java/com/controller/SampleController.java
+++ src/main/java/com/controller/SampleController.java
@@ -1,15 +1,30 @@
 package com.controller;
+
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SampleController {
 
-    @RequestMapping(value = "/", method = GET)
-    public String show() {
-        // jspのファイル名
-        return "test";
-    }
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@RequestMapping(value = "/", method = GET)
+	public String show() {
+
+		List<Map<String, Object>> result = jdbcTemplate.queryForList("select * from test");
+
+		result.forEach(x -> {
+			System.out.println(x);
+		});
+
+		return "test";
+	}
 }
